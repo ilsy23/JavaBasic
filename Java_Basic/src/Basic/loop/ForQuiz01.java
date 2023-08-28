52879deb787ff8a82256413e0ca52d296f288fb9
hansolL <ilsysli@naver.com>
2023-08-28 09:20:08 +0900

    23. 08. 25. 파일 누락으로 다시 커밋

---
+++ Java_Basic/src/Basic/loop/ForQuiz01.java
@@ -0,0 +1,19 @@
+package Basic.loop;
+
+public class ForQuiz01 {
+
+	public static void main(String[] args) {
+
+		//2~19까지의 난수를 생성해서 구구단을 출력해 보세요.(for)
+		//19행까지 출력하세요.
+		int dan = (int)(Math.random()*18+2);
+		
+		System.out.println("*** 구구단 " + dan + "단 ***");
+		System.out.println("-----------------");
+		for(int i=1; i<=19; i++) {
+			System.out.printf("%d × %d = %d\n", dan, i, dan*i);
+		}
+		
+	}
+
+}
